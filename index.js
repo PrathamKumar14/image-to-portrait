d89de5789e10043b5ffef51492e8a42170d36b07
Pratham kumar <prathamsolera@gmail.com>
2021-04-12 19:28:59 +0530

    Fix: word wrap issue

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 function loadFile(event) {
   const tempURL = URL.createObjectURL(event.target.files[0]);
   document.getElementById("text").style.backgroundImage = `url(${tempURL})`;
+  document.getElementById("input").style.visibility = "hidden";
+  document.getElementById("title").style.visibility = "hidden";
 }
 
 // Taking a screenshot
